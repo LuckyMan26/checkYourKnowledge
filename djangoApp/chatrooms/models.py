4127e2820698005a6c9ce71ed17de3c2e4953760
Lshprog <onisalex1@gmail.com>
2023-05-31 21:43:22 +0800

    Started classrooms implementation

--- djangoApp/chatrooms/models.py
+++ djangoApp/chatrooms/models.py
@@ -7,7 +7,7 @@ class Message(models.Model):
     author = models.ForeignKey(User, related_name='messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    def last_10_messages():
+    def last_10_messages(self):
         return Message.objects.order_by('-timestamp').all()[:10]
 
 class Task_model(models.Model):
@@ -22,3 +22,18 @@ class Task_model(models.Model):
     def last_10_tasks(class_room : int):
         tasks = Task_model.objects.all().filter(classroom_name=class_room)
         return tasks.order_by('-timestamp').all()[:10]
+
+
+class Classroom(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    join_code = models.TextField()
+    token = models.TextField()
+
+
+class ClassroomUserList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    classroom_id = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    role = models.TextField()
