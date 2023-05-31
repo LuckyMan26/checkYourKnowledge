f6c742f5cdb2f9e37db3cb549d0ed83ea3a92491
Lshprog <onisalex1@gmail.com>
2023-05-31 21:43:22 +0800

    Started classrooms implementation

--- djangoApp/chatrooms/models.py
+++ djangoApp/chatrooms/models.py
@@ -7,7 +7,7 @@ class Message(models.Model):
     author = models.ForeignKey(User, related_name='messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    def last_10_messages():
+    def last_10_messages(self):
         return Message.objects.order_by('-timestamp').all()[:10]
 
 class Task_model(models.Model):
@@ -15,4 +15,20 @@ class Task_model(models.Model):
     content_problem = models.TextField()
     content_answear = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    Id = models.TextField()
+    # Id = models.TextField()
+
+
+class Classroom(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    join_code = models.TextField()
+    token = models.TextField()
+
+
+class ClassroomUserList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    classroom_id = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    role = models.TextField()
+
--- djangoApp/chatrooms/urls.py
+++ djangoApp/chatrooms/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('create_classroom/', views.create_classroom, name='create_classroom'),
     path('<str:room_name>/', views.room, name='room'),
     path('<str:room_name>/createtask/', views.createtask, name='createtask'),
-    path('<str:room_name>/<str:task_id>/', views.task, name='task')
+    path('<str:room_name>/<str:task_id>/', views.task, name='task'),
+
 ]
